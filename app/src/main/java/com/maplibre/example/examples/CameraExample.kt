a5db6e0e96f4149abed4874e20e867338a33f470
Jacob Fielding <jf0517@gmail.com>
2024-07-16 17:08:03 -0700

    Corrected errors in pitch behavior

--- app/src/main/java/com/maplibre/example/examples/CameraExample.kt
+++ app/src/main/java/com/maplibre/example/examples/CameraExample.kt
@@ -1,5 +1,6 @@
 package com.maplibre.example.examples
 
+import android.graphics.Camera
 import android.location.Location
 import android.util.Log
 import androidx.compose.foundation.layout.Box
@@ -22,6 +23,7 @@ import com.maplibre.compose.MapView
 import com.maplibre.compose.StaticLocationEngine
 import com.maplibre.compose.camera.CameraState
 import com.maplibre.compose.camera.MapViewCamera
+import com.maplibre.compose.camera.cameraPaddingFractionOfScreen
 import com.maplibre.compose.camera.extensions.incrementZoom
 import com.maplibre.compose.camera.models.CameraPadding
 import com.maplibre.compose.rememberSaveableMapViewCamera
@@ -43,8 +45,10 @@ fun CameraExample() {
 
   val canChangeCamera = remember { mutableStateOf(false) }
 
+  val cameraPadding = cameraPaddingFractionOfScreen(top = 2.0f)
+  
   val mapViewCamera = rememberSaveableMapViewCamera() // Or rememberMapViewCamera()
-  val nextCameraState = getNextCamera(mapViewCamera.value.state)
+  val nextCameraState = getNextCamera(mapViewCamera.value.state, cameraPadding)
 
   // TODO: This could use improvement to handle failure cases.
   //      Not really in the scope of this project, but just to reduce
@@ -69,7 +73,9 @@ fun CameraExample() {
       Text(
           "Camera: ${mapViewCamera.value}",
           modifier =
-              Modifier.align(Alignment.TopCenter).padding(top = 16.dp, start = 16.dp, end = 16.dp),
+          Modifier
+            .align(Alignment.TopCenter)
+            .padding(top = 16.dp, start = 16.dp, end = 16.dp),
           fontSize = 11.sp,
           textAlign = TextAlign.Center)
 
@@ -80,14 +86,19 @@ fun CameraExample() {
               return@Button
             }
 
-            mapViewCamera.value = getNextCamera(mapViewCamera.value.state)
+            mapViewCamera.value = getNextCamera(mapViewCamera.value.state, cameraPadding)
           },
-          modifier = Modifier.align(Alignment.BottomStart).padding(bottom = 32.dp, start = 16.dp, end = 16.dp)) {
+          modifier =
+          Modifier
+            .align(Alignment.BottomStart)
+            .padding(bottom = 32.dp, start = 16.dp, end = 16.dp)) {
             Text("To ${nextCameraState.state}")
           }
 
       Column(
-          modifier = Modifier.align(Alignment.BottomEnd).padding(bottom = 150.dp, end = 16.dp),
+          modifier = Modifier
+            .align(Alignment.BottomEnd)
+            .padding(bottom = 150.dp, end = 16.dp),
           horizontalAlignment = Alignment.End) {
             Button(onClick = { mapViewCamera.value = mapViewCamera.value.incrementZoom(1.0) }) {
               Text("+")
@@ -101,17 +112,11 @@ fun CameraExample() {
   }
 }
 
-private fun getNextCamera(currentState: CameraState): MapViewCamera {
-  val padding = CameraPadding(
-      start = 0.0,
-      top = 500.0,
-      end = 0.0,
-      bottom = 0.0
-  )
-
+private fun getNextCamera(currentState: CameraState, padding: CameraPadding): MapViewCamera {
   return when (currentState) {
     is CameraState.Centered -> MapViewCamera.TrackingUserLocation(zoom = 18.0, pitch = 45.0)
-    is CameraState.TrackingUserLocation -> MapViewCamera.TrackingUserLocationWithBearing(zoom = 18.0, pitch = 45.0, padding = padding)
+    is CameraState.TrackingUserLocation ->
+        MapViewCamera.TrackingUserLocationWithBearing(zoom = 18.0, pitch = 45.0, padding = padding)
     is CameraState.TrackingUserLocationWithBearing -> MapViewCamera.Default
   }
 }
---
+++ compose/src/main/java/com/maplibre/compose/camera/runtime.kt
@@ -0,0 +1,38 @@
+package com.maplibre.compose.camera
+
+import androidx.annotation.FloatRange
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+import com.maplibre.compose.camera.models.CameraPadding
+
+/**
+ * Create a camera padding that's a fraction of the screen size.
+ *
+ * Important! This function can take values larger than 1.0 (need to investigate maplibre-native
+ * to determine what the acceptable range is).
+ *
+ * The default is 0.0f for all values.
+ *
+ * @param start The fraction of the screen width to pad on the start/left.
+ * @param top The fraction of the screen height to pad on the top.
+ * @param end The fraction of the screen width to pad on the end/right.
+ * @param bottom The fraction of the screen height to pad on the bottom.
+ * @return The camera padding.
+ */
+@Composable
+fun cameraPaddingFractionOfScreen(
+    @FloatRange(from = 0.0, to = 2.0) start: Float = 0.0f,
+    @FloatRange(from = 0.0, to = 2.0) top: Float = 0.0f,
+    @FloatRange(from = 0.0, to = 2.0) end: Float = 0.0f,
+    @FloatRange(from = 0.0, to = 2.0) bottom: Float = 0.0f
+): CameraPadding {
+  // Get the pixel dimensions of the screen. Dp is a density-independent pixel unit.
+  val screenHeight = LocalConfiguration.current.screenHeightDp.toDouble()
+  val screenWidth = LocalConfiguration.current.screenWidthDp.toDouble()
+
+  return CameraPadding(
+      start = start.toDouble() * screenWidth,
+      top = top.toDouble() * screenHeight,
+      end = end.toDouble() * screenWidth,
+      bottom = bottom.toDouble() * screenHeight)
+}
